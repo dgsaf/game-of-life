1d968480d61964e40dd59032a45e0a032981a643
Tom Ross <tom.ross@protonmail.com>
2021-04-16 12:41:53 +0800

    Included a function in gol-job-set-submission.sh to transform the parameter associative array into a string for exporting to gol-job-submission.sh

--- gol-job-set-submission.sh
+++ gol-job-set-submission.sh
@@ -16,7 +16,7 @@ boundary_type=0
 version_names="01_gol_cpu_serial_fort 02_gol_cpu_serial_fort"
 
 # parameter associative array with default values
-declare -a parameters
+declare -A parameters
 
 parameters[version_name]=""
 parameters[n_omp]=4
@@ -31,6 +31,7 @@ parameters[boundary_type]=0
 
 #
 echo "GOL SLURM job set submission"
+echo
 
 # GOL visualisation
 # Performing GOL simulations on a 10x10 grid, for 10 steps, with ascii
@@ -38,33 +39,52 @@ echo "GOL SLURM job set submission"
 # behaviour.
 echo "GOL ascii visualisation jobs"
 echo "versions: ${version_names}"
-echo "ngrid: ${parameters[grid_height]}x${parameters[grid_width]}"
-echo "nsteps: ${parameters[num_steps]}"
-echo "vis_type: ${parameters[visualisation_type]}"
+echo "ngrid: 10x10"
+echo "nsteps: 10"
+echo "vis_type: 3"
 
-for version_name in ${version_names}
-do
-    parameters[version_name]=version_name
+function parameter_string {
+    local -n array=$1
+
+    export_string=""
+
+    declare -i counter=1
+    length=${#array[*]}
+
+    for key in ${!array[*]} ; do
+        kv_pair="${key}=${array[${key}]}"
+        if [ ${counter} != ${length} ] ; then
+            export_string+="${kv_pair},"
+        else
+            export_string+="${kv_pair}"
+        fi
+        counter+=1
+    done
+    echo $export_string
+}
+
+for version_name in ${version_names} ; do
+    parameters[version_name]=${version_name}
     parameters[grid_height]=10
     parameters[grid_width]=10
     parameters[num_steps]=10
     parameters[visualisation_type]=3
+
+    parameter_string parameters
 done
-
-
 
 # # parameter list
 # parameter_list="\
-# version_name=${version_name},\
-# n_omp=${n_omp},\
-# grid_height=${grid_height},\
-# grid_width=${grid_width},\
-# num_steps=${num_steps},\
-# initial_conditions_type=${initial_conditions_type},\
-# visualisation_type=${visualisation_type},\
-# rule_type=${rule_type},\
-# neighbour_type=${neighbour_type},\
-# boundary_type=${boundary_type}"
+    # version_name=${version_name},\
+    # n_omp=${n_omp},\
+    # grid_height=${grid_height},\
+    # grid_width=${grid_width},\
+    # num_steps=${num_steps},\
+    # initial_conditions_type=${initial_conditions_type},\
+    # visualisation_type=${visualisation_type},\
+    # rule_type=${rule_type},\
+    # neighbour_type=${neighbour_type},\
+    # boundary_type=${boundary_type}"
 
 # # sbatch for
 # sbatch gol-job-submission.slurm --export=${parameter_list}
