30b766f7750a2280533c5ea459c12ddcf4cee532
Tom Ross <tom.ross@protonmail.com>
2021-04-16 13:04:46 +0800

    Included scaling tests in gol-job-set-submission.sh (3/?).

--- gol-job-set-submission.sh
+++ gol-job-set-submission.sh
@@ -1,19 +1,36 @@
 #!/bin/bash -l
 
-# default parameters
-version_name="01_gol_cpu_serial_fort"
-n_omp=2
-grid_height=10
-grid_width=10
-num_steps=10
-initial_conditions_type=0
-visualisation_type=0
-rule_type=0
-neighbour_type=0
-boundary_type=0
+# kv_string
+# utility function for converting parameter associative array into an export
+# string suitable for the command
+# > parameter_string=$(kv_string parameters)
+# > "sbatch gol-job-submission.slurm --export=${parameter_string}"
+function kv_string {
+    local -n array=$1
+
+    str=""
+
+    declare -i counter=1
+    length=${#array[*]}
+
+    for key in ${!array[*]} ; do
+        kv_pair="${key}=${array[${key}]}"
+        if [ ${counter} != ${length} ] ; then
+            str+="${kv_pair},"
+        else
+            str+="${kv_pair}"
+        fi
+        counter+=1
+    done
+
+    echo ${str}
+}
 
 # parameter sets
 version_names="01_gol_cpu_serial_fort 02_gol_cpu_serial_fort"
+# grid_sizes="2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384"
+grid_sizes="2 4 8 16 32 64 128 256 512 1024"
+omp_threads="1 2 3 4 5 6 7 8 9 10 11 12"
 
 # parameter associative array with default values
 declare -A parameters
@@ -29,7 +46,7 @@ parameters[rule_type]=0
 parameters[neighbour_type]=0
 parameters[boundary_type]=0
 
-#
+# GOL job sets
 echo "GOL SLURM job set submission"
 echo
 
@@ -42,27 +59,6 @@ echo "versions: ${version_names}"
 echo "ngrid: 10x10"
 echo "nsteps: 10"
 echo "vis_type: 3"
-
-function parameter_string {
-    local -n array=$1
-
-    export_string=""
-
-    declare -i counter=1
-    length=${#array[*]}
-
-    for key in ${!array[*]} ; do
-        kv_pair="${key}=${array[${key}]}"
-        if [ ${counter} != ${length} ] ; then
-            export_string+="${kv_pair},"
-        else
-            export_string+="${kv_pair}"
-        fi
-        counter+=1
-    done
-    echo $export_string
-}
-
 for version_name in ${version_names} ; do
     parameters[version_name]=${version_name}
     parameters[grid_height]=10
@@ -70,8 +66,36 @@ for version_name in ${version_names} ; do
     parameters[num_steps]=10
     parameters[visualisation_type]=3
 
-    parameter_string parameters
+    parameter_string=$(kv_string parameters)
+    sbatch gol-job-submission.slurm --export=${parameter_string}
 done
+
+# GOL scaling
+# Performing GOL simulations on increasing large grids, 2^n for n = 1, .., nmax,
+# for 100 steps, with no visualisation. Intended to determine the scaling
+# behaviour of the different GOL versions.
+echo "GOL scaling jobs"
+echo "versions: ${version_names}"
+echo "grid_sizes: ${grid_sizes}"
+echo "nsteps: 100"
+echo "vis_type: 0"
+for version_name in ${version_names} ; do
+    parameters[version_name]=${version_name}
+    parameters[num_steps]=100
+    parameters[visualisation_type]=0
+
+    for grid_size in ${grid_sizes}; do
+        parameters[grid_height]=${grid_size}
+        parameters[grid_width]=${grid_size}
+
+        parameter_string=$(kv_string parameters)
+        sbatch gol-job-submission.slurm --export=${parameter_string}
+    done
+done
+
+
+
+
 
 # # parameter list
 # parameter_list="\
