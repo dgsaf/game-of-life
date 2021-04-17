47e87cb68acafea60d24f7370a81041f0eca7b25
Tom Ross <tom.ross@protonmail.com>
2021-04-17 14:59:23 +0800

    Outlined report structure, included table of contents, sections and appendix. Modified listings.

--- report/auto/phys4004_assignment_1_openmp.el
+++ report/auto/phys4004_assignment_1_openmp.el
@@ -1,8 +1,6 @@
 (TeX-add-style-hook
  "phys4004_assignment_1_openmp"
  (lambda ()
-   (TeX-add-to-alist 'LaTeX-provided-class-options
-                     '(("article" "draft")))
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -12,14 +10,17 @@
     "listings")
    (LaTeX-add-labels
     "sec:Overview"
+    "sec:code-synopsis"
+    "sec:bash-scripts"
     "sec:serial-code"
     "sec:original-serial-code"
     "sec:improved-serial-code"
     "sec:profiling-comparison"
-    "sec:loop-parallel-code"
+    "sec:parallel-loop-code"
     "sec:results"
     "sec:unif-behav-verif"
     "sec:scaling-behaviour"
-    "sec:thre-indep-verif"))
+    "sec:thre-indep-verif"
+    "sec:appendix"))
  :latex)
 
