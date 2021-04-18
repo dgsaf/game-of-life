00ffaf70fcf53ed2fd67b40e14bb69d527eee3ed
Tom Ross <tom.ross@protonmail.com>
2021-04-18 12:42:22 +0800

    Added above-line comments on modifications to common_fort.f90 and 01_gol_cpu_serial_fort.f90 code, and propogated to 02_*.f90 code.

--- src/01_gol_cpu_serial_fort.f90
+++ src/01_gol_cpu_serial_fort.f90
@@ -123,6 +123,9 @@ subroutine game_of_life_stats(opt, step, current_grid)
     integer, dimension(:,:), intent(in) :: current_grid
     integer :: i, j, state
     integer*8 :: ntot
+    ! num_in_state and frac modified from dimension(NUMSTATES) to
+    ! dimension(0:NUMSTATES-1) to synchronise with the 0-based indexing of
+    ! states mandated in common_fort.f90
     integer, dimension(0:NUMSTATES-1) :: num_in_state
     real*4, dimension(0:NUMSTATES-1) :: frac
     character(len=30) :: fmt
@@ -147,6 +150,7 @@ subroutine game_of_life_stats(opt, step, current_grid)
 #endif
     end if
     write(10,*) "step ", step
+    ! loop bounds modified to synchronise with 0-based indexing of frac(:)
     do i = 0, NUMSTATES-1
         write(10,fmt, advance="no") "Frac in state ", i, " = ", frac(i), " "
     end do
